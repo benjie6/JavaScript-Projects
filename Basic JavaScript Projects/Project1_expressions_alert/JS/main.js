68fd6903086a285cebe080b76e6220c311f9edb5
benjamin <benjaminvega7@gmail.com>
2023-07-02 18:02:31 -0700

    update

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("Hello, World!");
+document.write('Hello, World!');
+document.write('example');
+window.alert("example2");
+document.write("remeber to say \"please\" and \"thank you\"")
+document.write("don\'t let yesterday take," + "up too much of today" + "- Will Rogers")
+var anime = "one piece", captain = "luffy", vicecaptain ="zoro", cook ="sanji";
+document.write(captain);
+document.write(5+5); //writting an expression 

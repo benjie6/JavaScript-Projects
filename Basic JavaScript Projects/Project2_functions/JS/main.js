a999283342133facf827b5b5115791d8149e5589
benjamin <benjaminvega7@gmail.com>
2023-07-03 15:35:03 -0700

    update

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function First_function() {
+    var name = "Benjamin"
+    var age = "22";
+    document.getElementById("output").innerHTML = "My name is " +name+ " I am " +age+ " years old.";
+}
+function myfunction(){
+    var sentence = "I am learning javascript";
+    sentence += " and alot from this course";
+    document.getElementById("concatenate").innerHTML = sentence;
+}

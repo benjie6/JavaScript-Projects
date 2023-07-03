a4ab0cacad9f964a23bd01058381a39acad32bac
benjamin <benjaminvega7@gmail.com>
2023-07-03 14:21:54 -0700

    update

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -7,3 +7,7 @@ document.write("don\'t let yesterday take," + "up too much of today" + "- Will R
 var anime = "one piece", captain = "luffy", vicecaptain ="zoro", cook ="sanji";
 document.write(captain);
 document.write(5+5); //writting an expression 
+var sent1 = "This is the beginning of the string"
+var sent2 = " and this is the end of the string"
+var concatenate = sent1 + sent2;
+document.write(concatenate);
